96559f42b4c380e04923b81ef6cd619d9b9096bf
Juan Salazar <ferthelet@gmail.com>
2023-04-23 08:33:38 -0500

    tries await and response.xxx() functions

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
+
 console.log('one');
 setTimeout(() => console.log('two'), 3000);
 console.log('three');
 
-console.log(fetch('http://localhost:3000/animals'));
+// console.log(fetch('http://localhost:3000/animals'));
 fetch('http://localhost:3000/animals')
     .then(response => console.log(response));
 
 async function logJSONData() {
+    // we have to use a proxy to get the Access-Control-Allow-Origin header added
     const response = await fetch('https://justcors.com/tl_dac7484');
     //const response = await fetch("http://cors-anywhere.com/http://example.com/movies.json");
     const jsonData = await response.json();
     console.log(jsonData);
 }
 
-logJSONData();
+// let response = fetch('http://localhost:3000/animals');
+// // await not working, only a the top level?
+// if (response.ok) {
+//     let json =  response.json();
+// } else {
+//     alert("HTTP error: " + response.status);
+// }
+
+// response can be
+// response.txt()
+// response.json()
+// response.formData()
+// response.blob() as a binary data type as a FormData() object
+// responseArrayBuffer() a two level representation of binary data
+// also response.arrayBody() is a readbale stream** to read the body chunk by chunk
