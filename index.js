ea12c5d1cceefaad1b722cfba304fcec55a487dd
Juan Salazar <ferthelet@gmail.com>
2023-04-23 08:16:03 -0500

    tries to deal with cors error, no luck

--- index.js
+++ index.js
@@ -4,4 +4,13 @@ console.log('three');
 
 console.log(fetch('http://localhost:3000/animals'));
 fetch('http://localhost:3000/animals')
-.then(response => console.log(response));
+    .then(response => console.log(response));
+
+async function logJSONData() {
+    const response = await fetch('https://justcors.com/tl_dac7484');
+    //const response = await fetch("http://cors-anywhere.com/http://example.com/movies.json");
+    const jsonData = await response.json();
+    console.log(jsonData);
+}
+
+logJSONData();
