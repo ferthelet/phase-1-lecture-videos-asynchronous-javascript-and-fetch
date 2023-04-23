7e10221fea55ee08e13b201c4c8472efab7e2f0e
Juan Salazar <ferthelet@gmail.com>
2023-04-23 09:09:48 -0500

    adds POST to animals successfully

--- await.js
+++ await.js
@@ -1,4 +1,7 @@
+// form https://javascript.info/fetch
+
 let url = 'https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits';
+let myUrl = "http:localhost:3000/animals"
 
 // without await works
 // fetch(url)
@@ -6,7 +9,7 @@ let url = 'https://api.github.com/repos/javascript-tutorial/en.javascript.info/c
 //   .then(commits => alert(commits[0].author.login));
 
 async function myAwait(url) {
-    let response = await fetch(url);
+    let response = await fetch(url); // no 2nd parameter? just a GET
     if(response.ok) {
         let commits = await response.json(); // read response body and parse as JSON
         console.log(commits[0].author.login);
@@ -15,5 +18,35 @@ async function myAwait(url) {
     }
 }
 
-
 myAwait(url);
+
+async function myAwaitGet(url) {
+    let response = await fetch(url); // no 2nd parameter? just a GET
+    if(response.ok) {
+        console.log(response);
+    } else {
+        alert("HTTP Error: " + response.status);
+    }
+}
+
+myAwaitGet(myUrl);
+
+async function myAwaitPost(url) {
+    let animal =    {
+        "name": "Striped Zebra",
+        "description": "Zebra can kick with the force of a mule",
+        "donations": 0
+    };
+    let response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8' 
+        },
+        body: JSON.stringify(animal)
+    });
+    let result = await response.json();
+    console.log(result.message);
+}
+
+myAwaitPost(myUrl);
+myAwaitGet(myUrl);
